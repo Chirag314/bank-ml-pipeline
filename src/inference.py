cdad7fd4f0679998b4cb9d7cefa65536c5880ce5
Chirag314 <chirag.d314@gmail.com>
2025-08-01 22:59:54 -0700

    Data added

--- src/inference.py
+++ src/inference.py
@@ -3,13 +3,18 @@ import joblib
 from sklearn.metrics import roc_auc_score
 from kaggle.api.kaggle_api_extended import KaggleApi
 
+
 def run_inference(model, X_test):
     preds = model.predict_proba(X_test)[:, 1]
-    submission = pd.read_csv("input/sample_submission.csv")
+    submission = pd.read_csv("data/sample_submission.csv")
     submission["target"] = preds
     submission.to_csv("submission.csv", index=False)
 
     # Submit to Kaggle
     api = KaggleApi()
     api.authenticate()
-    api.competition_submit(file_name="submission.csv", message="Auto submission", competition="your-competition-name")
+    api.competition_submit(
+        file_name="submission.csv",
+        message="Auto submission",
+        competition="your-competition-name",
+    )
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
+
 def load_data():
-    train = pd.read_csv("input/train.csv")
-    test = pd.read_csv("input/test.csv")
+    train = pd.read_csv("data/train.csv")
+    test = pd.read_csv("data/test.csv")
     y = train["target"]
     X = train.drop(columns=["target", "id"])
     X_test = test.drop(columns=["id"])
