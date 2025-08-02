74ca814da14a3d0772e369af2c45c0774356f5a2
Chirag314 <chirag.d314@gmail.com>
2025-08-01 23:17:45 -0700

    Added env secrets

--- src/utils.py
+++ src/utils.py
@@ -2,9 +2,9 @@ import pandas as pd
 
 
 def load_data():
-    train = pd.read_csv("data/train.csv")
-    test = pd.read_csv("data/test.csv")
-    y = train["target"]
-    X = train.drop(columns=["target", "id"])
+    train = pd.read_csv("src/data/train.csv")
+    test = pd.read_csv("src/data/test.csv")
+    y = train["y"]
+    X = train.drop(columns=["y", "id"])
     X_test = test.drop(columns=["id"])
     return X, y, X_test
